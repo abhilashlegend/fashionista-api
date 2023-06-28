d7dfc9cc44c54d66ad14685c0da9daf58ea99422
abhilashlegend <abhilash456a@live.com>
2023-06-28 15:43:30 +0530

    created Order model

---
+++ models/Order.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const schema = new Schema({
+    userid: {type: String, required: true},
+    orderdate: {type: Date, required: true},
+    address: {type: String, required: true},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+    pincode: {type: Number, required: true},
+    totalamount: {type: Number, required: true},
+    shipmentamount: {type: Number, required: true},
+    billamount: {type:Number, required: true},
+    status: {type: String, required: true},
+    products: []
+});
+
+let Order = mongoose.model("orders", schema);
+module.exports = Order;
